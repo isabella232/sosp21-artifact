2e1e04fa0345ebf73ce4f8f58db56db56319dd83
silveryfu <silveryfu@gmail.com>
2021-09-03 01:35:35 -0700

    add optional mock digi for benchmarks

--- benchmarks/digi-phy/room/driver/handler.py
+++ benchmarks/digi-phy/room/driver/handler.py
@@ -1,6 +1,9 @@
+import time
+
 import digi
 from digi import on, logger
 from digi.view import TypeView, DotView
+from digi.util import deep_get, deep_set, patch_spec
 
 _measure = ("bench.digi.dev", "v1", "measures", "measure-test", "default")
 _forward_set = False
@@ -22,6 +25,26 @@ def h(proc_view):
         for _l in active_lamps:
             room_brightness.status += _l.control.brightness.status
             _l.control.brightness.intent = room_brightness.intent / len(active_lamps)
+
+    # # benchmark
+    # global _forward_set
+    # if deep_get(proc_view, "control.brightness.intent") == 0.1 and not _forward_set:
+    #     resp, e = patch_spec(*_measure, {
+    #         "obs": {
+    #             "forward_root": time.time()
+    #         }
+    #     })
+    #     if e is None:
+    #         _forward_set = True
+    #
+    # global _backward_set
+    # if deep_get(proc_view, "control.brightness.status") == 0.1:
+    #     resp, e = patch_spec(*_measure, {
+    #         "obs": {
+    #             "backward_root": time.time()
+    #         }
+    #     })
+    #     _backward_set = True
 
 
 if __name__ == '__main__':
--- benchmarks/digis/lamp/driver/handler.py
+++ benchmarks/digis/lamp/driver/handler.py
@@ -5,7 +5,6 @@ from digi.util import deep_get, patch_spec, deep_set
 
 import time
 import threading
-import lifx
 
 _poll = None
 _dev = None
@@ -13,33 +12,6 @@ _dev = None
 _measure = ("bench.digi.dev", "v1", "measures", "measure-test", "default")
 _forward_set = False
 _backward_set = False
-
-# source: https://github.com/mclarkk/lifxlan
-# power can be "on"/"off", True/False, 0/1, or 0/65535
-# color is a HSBK list of values: [hue (0-65535), saturation (0-65535), brightness (0-65535), Kelvin (2500-9000)]
-# duration is the transition time in milliseconds
-# rapid is True/False. If True, don't wait for successful confirmation, just send multiple packets and move on
-# is_transient is 1/0. If 1, return to the original color after the specified number of cycles. If 0,
-# set light to specified color
-# period is the length of one cycle in milliseconds
-# cycles is the number of times to repeat the waveform
-# duty_cycle is an integer between -32768 and 32767. Its effect is most obvious with the Pulse waveform
-#     set duty_cycle to 0 to spend an equal amount of time on the original color and the new color
-#     set duty_cycle to positive to spend more time on the original color
-#     set duty_cycle to negative to spend more time on the new color
-# waveform can be 0 = Saw, 1 = Sine, 2 = HalfSine, 3 = Triangle, 4 = Pulse (strobe)
-# infrared_brightness (0-65535) - is the maximum infrared brightness when the lamp automatically
-# turns on infrared (0 = off)
-convert = {
-    "power": {
-        "from": lambda x: "on" if x > 0 else "off",
-        "to": lambda x: 65535 if x == "on" else 0,
-    },
-    "brightness": {
-        "from": lambda x: round(x / 65535, 2),
-        "to": lambda x: x * 65535,
-    }
-}
 
 
 class _Poll(threading.Thread):
@@ -54,12 +26,7 @@ class _Poll(threading.Thread):
             if self.stop_flag:
                 break
 
-            status = lifx.get(self.dev)
-            if status is None:
-                continue
-            p, b = status.get("power", {}), status.get("color", {})[2]
-            p, b = convert["power"]["from"](p), convert["brightness"]["from"](b)
-
+            global p, b
             patch = {
                 "control": {
                     "power": {
@@ -89,20 +56,11 @@ class _Poll(threading.Thread):
             time.sleep(self.interval)
 
 
-# status
+# meta
 @on.meta
 def h0(sv):
     e = sv.get("endpoint", None)
     if e is None:
-        return
-
-    global _dev
-    for _ in range(sv.get("discover_retry", 3)):
-        _dev = lifx.discover(e)
-        if _dev is not None:
-            break
-
-    if _dev is None:
         return
 
     if _poll is not None:
@@ -131,19 +89,10 @@ def h1(sv, pv):
         if e is None:
             _forward_set = True
 
-    status = lifx.get(_dev)
-    power = status.get("power", 0)
-    color = list(status.get("color", [1, 1, 1, 1]))
-
+    global p, b
     p, b = deep_get(sv, "power.intent"), deep_get(sv, "brightness.intent")
-
-    if p is not None:
-        power = convert["power"]["to"](p)
-    if b is not None:
-        color[2] = convert["brightness"]["to"](b)
-
-    # benchmark
-    lifx.put(_dev, power, color)
+    deep_set(sv, "power.status", p)
+    deep_set(sv, "brightness.status", b)
 
 
 if __name__ == '__main__':
